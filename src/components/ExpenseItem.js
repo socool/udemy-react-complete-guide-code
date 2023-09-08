8e91d6ea4bd1291c9c83f04b333cc80c9f134bd9
So Cool <127.0.0.1.pm@gmail.com>
2023-09-08 13:09:36 +0700

    add content for ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2>ExpenseItem</h2>;
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>283$</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
