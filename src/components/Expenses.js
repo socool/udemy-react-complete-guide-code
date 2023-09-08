b3c4a4c12419444d52d6c63f781714b416c09ec9
So Cool <127.0.0.1.pm@gmail.com>
2023-09-08 15:42:26 +0700

    change to loop and add key for map

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -2,27 +2,18 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 function Expenses(props) {
+  const expenses = props.expenses;
   return (
     <div className="expenses">
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </div>
   );
 }
-
 export default Expenses;
