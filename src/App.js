48818f35cbfeb34b6fc05a61aa1af7628bb52818
So Cool <127.0.0.1.pm@gmail.com>
2023-09-08 11:31:50 +0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 function App() {
+  const para = document.createElement("p");
+  para.textContent = "Hello World!";
+  document.getElementById("root").appendChild(para);
   return (
     <div>
       <h2>Let's get started!</h2>
