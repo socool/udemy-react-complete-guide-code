817521af34973f8a08beede9436d9d4710c8ed45
So Cool <127.0.0.1.pm@gmail.com>
2023-09-12 11:52:41 +0700

    clicked but not Updated

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,14 +4,16 @@ import ExpenseDate from "./ExpenseDate";
 import React from "react";
 
 function ExpenseItem(props) {
+  let title = props.title;
   const clickHandler = () => {
     console.log("Clicked");
+    title = "Updated";
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
         <button onClick={clickHandler}>Change Title</button>
       </div>
