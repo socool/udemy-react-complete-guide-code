4099ea653fafe09c50dbfc594e0dc2275ea7ae49
So Cool <127.0.0.1.pm@gmail.com>
2023-09-12 16:41:03 +0700

    derived / computed state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,6 +8,18 @@ function Expenses(props) {
   const [filterYear, setFilterYear] = useState("2023");
   const expenses = props.expenses;
 
+  let filterInfoText = "2019,2021 & 2022";
+
+  if (filterYear === "2019") {
+    filterInfoText = "2020, 2021 & 2022";
+  } else if (filterYear === "2020") {
+    filterInfoText = "2019, 2021 & 2022";
+  } else if (filterYear === "2021") {
+    filterInfoText = "2019, 2020 & 2022";
+  } else {
+    filterInfoText = "2019, 2020 & 2021";
+  }
+
   const filterChangeHandler = (selectedYear) => {
     console.log("in Expenses.js");
     console.log(selectedYear);
@@ -21,6 +33,7 @@ function Expenses(props) {
           selected={filterYear}
           onChangeFilter={filterChangeHandler}
         />
+        <p>Data for years {filterInfoText} is hidden.</p>
         {expenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
