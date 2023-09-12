67cce50cdefb50f7258d49c00e357311c617dc2d
So Cool <127.0.0.1.pm@gmail.com>
2023-09-12 16:28:30 +0700

    excercise working with events and state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,19 +1,35 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
-import React from "react";
+import React, { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
+import Card from "../UI/Card";
 
 function Expenses(props) {
+  const [filterYear, setFilterYear] = useState("2023");
   const expenses = props.expenses;
+
+  const filterChangeHandler = (selectedYear) => {
+    console.log("in Expenses.js");
+    console.log(selectedYear);
+    setFilterYear(selectedYear);
+  };
+
   return (
-    <div className="expenses">
-      {expenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filterYear}
+          onChangeFilter={filterChangeHandler}
         />
-      ))}
+        {expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
     </div>
   );
 }
