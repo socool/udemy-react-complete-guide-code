a8513da11fc84c03486a8a772fb133e225f96fa6
So Cool <127.0.0.1.pm@gmail.com>
2023-09-12 14:03:10 +0700

    using one state instead

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,20 +2,37 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState("");
+  //const [enteredTitle, setEnteredTitle] = useState("");
+  //const [enteredAmount, setEnteredAmount] = useState("");
+  //const [enteredDate, setEnteredDate] = useState("");
+
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
+
   const titleChangeHandler = (event) => {
-    console.log(event.target.value);
-    setEnteredTitle(event.target.value);
+    //console.log(event.target.value);
+    //setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput, //use spread operator for separating data in arrays to individual elements
+      enteredTitle: event.target.value,
+    });
   };
 
-  const [enteredAmount, setEnteredAmount] = useState("");
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    });
   };
 
-  const [enteredDate, setEnteredDate] = useState("");
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
   };
 
   useState("");
